bd06d23862b989aa44452ccce3293903184921b5
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-05-28 22:19:21 +0530

    feat:polls module scaffolding

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PollsController } from './polls/polls.controller';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [PollsModule],
+  controllers: [AppController, PollsController],
   providers: [AppService],
 })
 export class AppModule {}
