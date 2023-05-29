6339104d785d334e0dd40be80c926e80c763117e
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-05-29 17:07:23 +0530

    feat: redis module setup completed

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { REDISModule } from 'src/config.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, REDISModule ],
   controllers: [PollsController],
   providers: [PollsService],
 })
