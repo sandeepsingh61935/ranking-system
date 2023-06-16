510f42864bde31a1e859b90f9cd41eb8ca39f7eb
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-06-16 18:39:43 +0530

    feat: wating room and nomination form setup is done

--- client/src/state.ts
+++ client/src/state.ts
@@ -35,48 +35,53 @@ export type AppState = {
     accessToken?: string;
     socket?: Socket;
     wsErrors: WsErrorUnique[];
+    isAdmin: boolean;
+    nominationCount: number;
+    participantCount: number;
+    canStartVote: boolean;
 }
 
-const state: AppState = proxy({
+const state: AppState = proxy<AppState>({
     isLoading: false,
     currentPage: AppPage.Welcome,
-});
+    wsErrors: [],
+    get me() {
+        const accessToken = this.accessToken;
 
-const deriveState: AppState = derive(
-    {
-        me: (get) => {
-            const accessToken = get(state).accessToken;
+        if (!accessToken) {
+            return;
+        }
 
-            if (!accessToken) {
-                return;
-            }
+        const token = getTokenPayload(accessToken);
 
-            const token = getTokenPayload(accessToken);
-
-            return {
-                id: token.sub,
-                name: token.name,
-            };
-        },
-        isAdmin: (get) => {
-            if (!get(state).me) {
-                return false;
-            }
-            return get(state).me?.id === get(state).poll?.adminID;
-        },
+        return {
+            id: token.sub,
+            name: token.name,
+        };
     },
-    {
-        proxy: state,
-    }
-)
+    get isAdmin() {
+        if (!this.me) {
+            return false;
+        }
+        return this.me?.id === this.poll?.adminID;
+    },
+    get participantCount() {
+        return Object.keys(this.poll?.participants || {}).length;
+    },
+    get nominationCount() {
+        return Object.keys(this.poll?.nominations || {}).length;
+    },
+    get canStartVote() {
+        const votesPerVoter = this.poll?.votesPerVoter ?? 100;
+
+        return this.nominationCount >= votesPerVoter;
+    },
+});
 
 
 const actions = {
     setPage: (page: AppPage): void => {
         state.currentPage = page;
-    },
-    startOver: (): void => {
-        actions.setPage(AppPage.Welcome);
     },
     startLoading: (): void => {
         state.isLoading = true;
@@ -106,6 +111,31 @@ const actions = {
     updatePoll: (poll: Poll): void => {
         state.poll = poll;
     },
+    nominate: (text: string): void => {
+        state.socket?.emit('nominate', { text });
+    },
+    startOver: (): void => {
+        actions.reset();
+        localStorage.removeItem('accessToken');
+        actions.setPage(AppPage.Welcome);
+    },
+    reset: (): void => {
+        state.socket?.disconnect();
+        state.poll = undefined;
+        state.accessToken = undefined;
+        state.isLoading = false;
+        state.socket = undefined;
+        state.wsErrors = [];
+    },
+    removeNomination: (id: string): void => {
+        state.socket?.emit('remove_nomination', { id });
+    },
+    removeParticipant: (id: string): void => {
+        state.socket?.emit('remove_participant', { id });
+    },
+    startVote: (): void => {
+        state.socket?.emit('start_vote');
+    },
     addWsError: (error: WsError): void => {
         state.wsErrors = [
             ...state.wsErrors,
@@ -127,4 +157,4 @@ subscribeKey(state, 'accessToken', () => {
 });
 
 export type AppActions = typeof actions;
-export { deriveState as state, actions };
+export { state, actions };
