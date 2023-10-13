145e1029f75e85e5838cfd69036fb46403e8b426
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-10-13 20:25:25 +0530

    modify prod ecosystem config of client

--- client/prod.ecosystem.config.cjs
+++ client/prod.ecosystem.config.cjs
@@ -1,13 +1,14 @@
 module.exports = {
-  "apps": [
+  apps: [
     {
-      "name": "frontend",
-      "script": "serve",
-      "env": {
-        "PM2_SERVE_PATH": "dist",
-        "PM2_SERVE_PORT": 3000,
-        "PM2_SERVE_SPA": "true",
-        "PM2_SERVE_HOMEPAGE": "/index.html"
+      name: 'frontend',
+      script: 'vite',
+      args: 'serve',
+      env: {
+        PM2_SERVE_PATH: './dist',
+        PM2_SERVE_PORT: 3000,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
       }
     }
   ],
@@ -17,9 +18,9 @@ module.exports = {
       host : '172.31.84.100',
       ref  : 'origin/master',
       repo : 'git@github.com:sandeepsingh61935/ranking-system.git',
-      path : './dist/index.html',
+      path : './dist',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env .env',
       'pre-setup': ''
     }
   }
