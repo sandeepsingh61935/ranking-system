b8b3bc061129e0f781f1bea3b65858dac557b1cc
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-10-17 10:55:23 +0530

    enable cors

--- server/src/socket.io.adapter.ts
+++ server/src/socket.io.adapter.ts
@@ -23,7 +23,7 @@ export class SocketIOAdapter extends IoAdapter {
         `http://localhost:${clientPort}`,
         `http://127.0.0.1:${clientPort}`,
         `http://peakrater.duckdns.org:${clientPort}`,
-        `54.144.198.82:${clientPort}`
+        `http://54.144.198.82:${clientPort}`
       ],
       methods: ["POST"]
     };
@@ -33,7 +33,8 @@ export class SocketIOAdapter extends IoAdapter {
     });
 
     const optionsWithCORS: ServerOptions = {
-      ...options
+      ...options,
+      cors
     };
 
     const jwtService = this.app.get(JwtService);
