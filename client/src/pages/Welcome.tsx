eca8eb7e7e9283b0338d17bd2a4df56a880e8788
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-10-26 11:30:56 +0530

    change welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -4,7 +4,8 @@ import { AppPage, actions } from '../state';
 const Welcome: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full">
-      <h1 className="text-center my-12">Welcome to Ranking system</h1>
+      <h1 className="text-center my-12">Welcome to Peak Rater</h1>
+      
       <div className="my-12 flex flex-col justify-center">
         <button
           className="box btn-orange my-2"
